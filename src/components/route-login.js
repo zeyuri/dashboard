b678d1507ff202a89641307559eaf0da69270132
Zé Yuri <jozeyuri@gmail.com>
2020-04-07 21:44:07 -0300

    auth tuto

---
+++ src/components/route-login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const RouteLogin = () => (
+  <>
+    <h1>Log In or Sign Up</h1>
+    <button>log in</button>
+  </>
+)
+
+export default RouteLogin
---
+++ src/components/route-secret.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const RouteSecret =() => <h1>Super Secret Stuff</h1>
+
+export default RouteSecret
